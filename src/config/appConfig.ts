214dce143d058f89db9d589b8618587c1e7b7726
MD ABIR MAHMUD <abirmahmud5665@gmail.com>
2026-04-26 20:29:34 +0600

    feat: add new variables

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -1,3 +1,4 @@
+import { coreAppConfigZodSchema } from "@havendor/server-core";
 import dotenv from "dotenv";
 import path from "path";
 import { z } from "zod";
@@ -5,24 +6,11 @@ import { z } from "zod";
 dotenv.config({ path: path.join(process.cwd(), ".env") });
 
 const envVarsZodSchema = z.object({
-  NODE_ENV: z.enum(["development", "production", "test"]),
-  PORT: z
-    .string({ error: "PORT is required" })
-    .default("3005")
-    .transform((val) => Number(val)),
   CORS_ALLOWED_ORIGINS: z.string().default(`["http://localhost:3001"]`),
-  DATABASE_URL: z.string({ error: "DATABASE_URL is required" }),
-  ENCRYPTION_KEY: z.string({ error: "ENCRYPTION_KEY is required" }),
-  HASH_PEPPER: z.string({ error: "HASH_PEPPER is required" }),
-  REDIS_URL: z.string({ error: "REDIS_URL is required" }),
-  SERVICE_NAME: z.string({ error: "SERVICE_NAME is required" }),
-  ENABLE_FILE_LOGGING: z
-    .string()
-    .default("true")
-    .transform((val) => val === "true"),
+  DATABASE_URL: z.url({ error: "DATABASE_URL is required" }),
 });
 
-const envVars = envVarsZodSchema.parse(process.env);
+const envVars = z.intersection(envVarsZodSchema, coreAppConfigZodSchema).parse(process.env);
 
 const appConfig = {
   NODE_ENV: envVars.NODE_ENV,
@@ -36,6 +24,7 @@ const appConfig = {
   REDIS_URL: envVars.REDIS_URL,
   SERVICE_NAME: envVars.SERVICE_NAME,
   ENABLE_FILE_LOGGING: envVars.ENABLE_FILE_LOGGING,
+  INTERNAL_SERVICE_SECRET: envVars.INTERNAL_SERVICE_SECRET,
 } as const;
 
 export default appConfig;

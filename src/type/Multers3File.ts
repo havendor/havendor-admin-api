5f883c25e8df59831adac0684d57e595d190dcc9
MD ABIR MAHMUD <abirmahmud5665@gmail.com>
2026-05-20 17:05:24 +0600

    feat: add multer s3 file type

---
+++ src/type/Multers3File.ts
@@ -0,0 +1,19 @@
+export type MulterS3File = {
+  fieldname: string;
+  originalname: string;
+  encoding: string;
+  mimetype: string;
+  size: number;
+  bucket: string;
+  key: string;
+  acl: string;
+  contentType: string;
+  contentDisposition: string | null;
+  contentEncoding: string | null;
+  storageClass: string;
+  serverSideEncryption: string | null;
+  metadata: unknown;
+  location: string;
+  etag: string;
+  versionId: string;
+};
--- src/type/index.ts
+++ src/type/index.ts
@@ -1,3 +1,4 @@
 export type * from "./email";
+export type * from "./Multers3File";
 export type * from "./prisma";
 export type * from "./TJWTPayload";

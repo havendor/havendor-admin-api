a57a8463b8bc009c08ce32323f097a0f503cb98d
MD ABIR MAHMUD <abirmahmud5665@gmail.com>
2026-05-20 17:07:31 +0600

    chore: add custom files type and validated prop on request

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,6 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/consistent-type-definitions */
 import "express-serve-static-core";
 import { TAdminCache } from "./modules/admin/admin/admin.type";
+import { MulterS3File } from "./type/Multers3File";
 
 declare module "express-serve-static-core" {
   interface Request {
@@ -8,5 +10,13 @@ declare module "express-serve-static-core" {
     userAgent?: string;
     tenantId?: string;
     admin?: TAdminCache;
+    files?: MulterS3File[];
+    file?: MulterS3File;
+    validated?: {
+      body?: any;
+      query?: any;
+      params?: any;
+      cookies?: any;
+    };
   }
 }

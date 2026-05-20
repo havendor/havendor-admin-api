7c897cf89109004fc6541ca8256077beb08a2cc2
MD ABIR MAHMUD <abirmahmud5665@gmail.com>
2026-05-20 16:59:15 +0600

    feat: add role and admin route

--- src/routes/admin-routes.ts
+++ src/routes/admin-routes.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
+import { AdminRoute } from "../modules/admin/admin/admin.route.js";
 import { AuthRoutes } from "../modules/admin/auth/auth.route.js";
+import { RoleRoute } from "../modules/admin/role/role.route.js";
 import { routeRateConfig } from "../utility/index.js";
 
 const router: Router = Router();
@@ -8,6 +10,14 @@ const routes: { path: string; route: Router }[] = [
   {
     path: "/auth",
     route: AuthRoutes,
+  },
+  {
+    path: "/admin",
+    route: AdminRoute,
+  },
+  {
+    path: "/role",
+    route: RoleRoute,
   },
 ];
 
